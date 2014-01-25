b57f97b27f3d34cde6fa559af9e82467c1599358
Joey Tuong <joey@tetrap.us>
2014-01-25 21:57:02 +1100

    yo first commit

---
+++ brython/libs/javascript.js
@@ -0,0 +1,5 @@
+$module = {
+    JSObject:__BRYTHON__.JSObject,
+    JSConstructor:__BRYTHON__.JSConstructor,
+    console: __BRYTHON__.JSObject(window.console)
+}
---
+++ test.py
@@ -0,0 +1,3 @@
+from browser import doc
+def echo():
+    alert(doc["zone"].value)
